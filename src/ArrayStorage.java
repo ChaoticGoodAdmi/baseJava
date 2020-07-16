2571ede24a36aebf790d86dac1f0c70a4633646c
ChaoticGoodAdmi <imonare@gmail.com>
2020-07-16 16:15:09 +0300

    Первая рабочая версия с реализованными методами get, save, delete, clear и getAll

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,59 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        return null;
+        int id = findResumeID(uuid);
+        if (id == -1) {
+            return null;
+        } else {
+            return storage[id];
+        }
     }
 
     void delete(String uuid) {
+        int id = findResumeID(uuid);
+        if (id == size - 1) {
+            storage[id] = null;
+        } else {
+            storage[id] = storage[size - 1];
+            storage[size - 1] = null;
+        }
+        size--;
+    }
+
+    private int findResumeID(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] cvs = new Resume[size];
+        System.arraycopy(storage, 0, cvs, 0, size);
+        return cvs;
     }
 
     int size() {
-        return 0;
+        return this.size;
     }
 }
