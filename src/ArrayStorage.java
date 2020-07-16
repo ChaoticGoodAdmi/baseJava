39fd1e807ff0206f691402251959e0471015684d
ChaoticGoodAdmi <imonare@gmail.com>
2020-07-16 18:00:16 +0300

    Исправлен метод get для несуществующих резюме

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,28 +2,32 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    int maxVolume = 2;
-    int expansionRate = 2;
-    Resume[] storage = new Resume[maxVolume];
-    int storageSize = 0;
+    private int maxVolume = 10000;
+    private final int EXPANSION_RATE = 2;
+    private Resume[] storage = new Resume[maxVolume];
+    private int storageSize = 0;
 
-    void clear() {
+    void clear () {
         for (int i = 0; i < storageSize; i++) {
             storage[i] = null;
         }
         storageSize = 0;
     }
 
-    void save(Resume resume) {
+    void save (Resume resume) {
         if (storageSize == maxVolume) {
             expandStorage();
         }
-        storage[storageSize] = resume;
-        storageSize++;
+        if (findResumeID(resume.uuid) == -1) {
+            storage[storageSize] = resume;
+            storageSize++;
+        } else {
+            System.out.println("Резюме с таким uuid уже добавлено");
+        }
     }
 
-    private void expandStorage() {
-        int expandedSize = this.storageSize * this.expansionRate;
+    private void expandStorage () {
+        int expandedSize = this.storageSize * this.EXPANSION_RATE;
         Resume[] expandedStorage = new Resume[expandedSize];
         System.arraycopy(this.storage, 0, expandedStorage, 0, storageSize);
         this.storage = expandedStorage;
@@ -31,27 +35,34 @@ public class ArrayStorage {
         System.out.println("Размер хранилища увеличен до " + maxVolume);
     }
 
-    Resume get(String uuid) {
+    Resume get (String uuid) {
         int id = findResumeID(uuid);
         if (id == -1) {
-            return null;
+            Resume dummyResume = new Resume();
+            dummyResume.uuid = "< не найдено >";
+            return dummyResume;
         } else {
             return storage[id];
         }
     }
 
-    void delete(String uuid) {
+    void delete (String uuid) {
         int id = findResumeID(uuid);
-        if (id == storageSize - 1) {
-            storage[id] = null;
+        if (id != -1) {
+            if (id == storageSize - 1) {
+                storage[id] = null;
+            } else {
+                storage[id] = storage[storageSize - 1];
+                storage[storageSize - 1] = null;
+            }
+            storageSize--;
         } else {
-            storage[id] = storage[storageSize - 1];
-            storage[storageSize - 1] = null;
+            System.out.println("Не найдено такое резюме");
         }
-        storageSize--;
+
     }
 
-    private int findResumeID(String uuid) {
+    private int findResumeID (String uuid) {
         for (int i = 0; i < storageSize; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return i;
@@ -63,13 +74,13 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    Resume[] getAll () {
         Resume[] cleanResumeStorage = new Resume[storageSize];
         System.arraycopy(storage, 0, cleanResumeStorage, 0, storageSize);
         return cleanResumeStorage;
     }
 
-    int size() {
+    int size () {
         return this.storageSize;
     }
 }
