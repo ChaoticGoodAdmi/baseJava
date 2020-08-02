ab10ed872840d236dcffdba4e71cb951a5849c6c
ChaoticGoodAdmi <imonare@gmail.com>
2020-08-02 17:47:10 +0300

    Lesson 06: усовершенствован компаратор Resume, добавлен класс для общего тестирования

--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertTrue;
 
 public abstract class AbstractStorageTest {
 
-    AbstractStorage storage;
+    protected AbstractStorage storage;
     private static final String UUID_1 = "UUID_1";
     private static final String UUID_2 = "UUID_2";
     private static final String UUID_3 = "UUID_3";
