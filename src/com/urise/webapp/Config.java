450fb0f8303f3753793fc6e8865c41a9bd77df3e
ChaoticGoodAdmi <imonare@gmail.com>
2020-09-22 00:11:11 +0300

    Lesson 16 added heroku properties

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -4,15 +4,13 @@ import com.urise.webapp.storage.SqlStorage;
 import com.urise.webapp.storage.Storage;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
 
-    private static final File PROPS = new File(System.getProperty("user.home") +
-            "\\IdeaUEProjects\\basejava\\config\\resumes.properties");
+    private static final String PROPS = "/resumes.properties";
     private static final Config INSTANCE = new Config();
 
     private final Storage storage;
@@ -23,7 +21,7 @@ public class Config {
     }
 
     private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
             Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
@@ -32,7 +30,7 @@ public class Config {
                     props.getProperty("db.user"),
                     props.getProperty("db.password"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file " + PROPS);
         }
     }
 
